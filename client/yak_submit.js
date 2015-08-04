00635a16653de022131dce7f5a1543b4c28ca221
evanjon <ejgennrich@gmail.com>
2015-08-04 15:44:53 -0400

    fixed some file strcutre, added some styling, and added the title to the yak post

--- client/yak_submit.js
+++ client/yak_submit.js
@@ -1,17 +1,34 @@
-Template.yaksSubmit.events({
-	'submit .yaksSubmitForm': function(event,err) {
+// Template.yaksSubmit.events({
+// 	'submit .yaksSubmitForm': function(event,err) {
 
-		event.preventDefault();
-		var yak = event.target.yak.value; 		// get yak value
+// 		event.preventDefault();
+// 		var postTitle = event.target.postTitle.value; 		// get yakTitle value
+// 		var yak = event.target.yak.value; 		// get yak value
 
-		// check if the value is empty
-		if (yak == "") {
-			alert("You can't insert empty yak. Try to write something funny instead! :)");
-		} else {
-			Meteor.call('yakInsert', yak);
-			/*post._id = Yaks.insert(post);*/
-			Router.go('yaksList');
-		}
+// 		// check if the value is empty
+// 		if (yak == "") {
+// 			alert("Post details");
+// 		} else {
+// 			Meteor.call('yakInsert', yak);
+// 			Meteor.call('postTitleInsert', postTitle)
+// 			/*post._id = Yaks.insert(post);*/
+// 			Router.go('yaksList');
+// 		}
 		
-	}
+// 	}
+// });
+
+
+Template.yaksSubmit.events({
+'submit form': function(e) {
+e.preventDefault();
+
+var post = {
+postTitle: $(e.target).find('[name=postTitle]').val(),
+yak: $(e.target).find('[name=yak]').val()
+};
+
+post._id = Yaks.insert(post);
+Router.go('yaksList', post);
+}
 });
